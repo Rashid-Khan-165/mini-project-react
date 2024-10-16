ecbf890b505a89544eeec958cb99c8ede26e34f5
Rashid Khan <williamsonkane7539@gmail.com>
2024-10-16 12:39:27 +0530

    First Commit

---
+++ mini-project-react/src/App.jsx
@@ -0,0 +1,13 @@
+// import './App.css'
+import TextField from '@mui/material/TextField';
+import SearchBox from "./SearchBox"
+
+function App() {
+  return (
+    <>
+      <SearchBox/>
+    </>
+  )
+}
+
+export default App
