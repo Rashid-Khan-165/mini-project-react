3436a9cb89b86890d445c1a1034fe12b2a564c2b
Rashid Khan <williamsonkane7539@gmail.com>
2024-10-17 15:02:44 +0530

    Creating SearchBox

--- mini-project-react/src/SearchBox.jsx
+++ mini-project-react/src/SearchBox.jsx
@@ -3,9 +3,28 @@ import Button from '@mui/material/Button';
 import "./SearchBox.css";
 import { useState } from 'react';
 
-export default function SearchBox() {
-      let [city,setCity] = useState("");
+export default function SearchBox() { 
+  let [city,setCity] = useState("");
 
+      const API_URL = "https://api.openweathermap.org/data/2.5/weather"
+      const API_KEY ="825bf5214f26cf35c5c1a3dba5f435cc";
+      
+      let getWeatherInfo = async() => {
+        let response = await fetch (`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`)
+        let jsonResponse = await response.json();
+        console.log(jsonResponse);
+        let result = {
+          temp: jsonResponse.main.temp,
+          tempMin : jsonResponse.main.temp_min,
+          tempmax : jsonResponse.main.temp_max,
+          humidity : jsonResponse.main.humidity,
+          feelsLike : jsonResponse.main.feels_like,
+          weather : jsonResponse.weather[0].description
+        };
+        console.log(result);
+      }
+
+      
       let handleChange = (evt) => {
         setCity(evt.target.value);
       }
@@ -14,6 +33,7 @@ export default function SearchBox() {
         evt.preventDefault();
         console.log(city);
         setCity("");
+        getWeatherInfo();
       }
 
     return (
